0836cc5a8c37e4690eaba1918e82ef04eb2065bd
shan hsu <ppearl0329tw@gmail.com>
2021-05-11 18:41:36 +0800

    安裝: tailwind and fontawesome

---
+++ app/javascript/icon/fontawesome.js
@@ -0,0 +1,8 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faPen, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPen, faLightbulb)
+dom.watch()
+
+/* <i class="fas fa-lightbulb"></i>
+<i class="fas fa-pen"></i>*/
