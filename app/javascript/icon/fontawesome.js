ac417367a70298dfd440bef731aceb84d3cf4db9
shan hsu <ppearl0329tw@gmail.com>
2021-05-12 16:44:42 +0800

    新增: 專案環境設定-fontawesome & tailwindcss

--- app/javascript/icon/fontawesome.js
+++ app/javascript/icon/fontawesome.js
@@ -4,5 +4,3 @@ import { faPen, faLightbulb } from '@fortawesome/free-solid-svg-icons'
 library.add(faPen, faLightbulb)
 dom.watch()
 
-/* <i class="fas fa-lightbulb"></i>
-<i class="fas fa-pen"></i>*/
