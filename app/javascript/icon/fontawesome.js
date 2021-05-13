e136b164f7787e8a185a2f37aacbdf4f67019fd6
shan <80436345+shan-0311@users.noreply.github.com>
2021-05-13 14:06:58 +0800

    Features/project setup (#15)
    * 安裝: tailwind and fontawesome
    
    * 新增: 專案環境設定-fontawesome & tailwindcss
    
    PS. 內含Test相關檔案可以讓各位測試功能是否正常，未來上線合併至main分支前會予以刪除。

---
+++ app/javascript/icon/fontawesome.js
@@ -0,0 +1,6 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faPen, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPen, faLightbulb)
+dom.watch()
+
