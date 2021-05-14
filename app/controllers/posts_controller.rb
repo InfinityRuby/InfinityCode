6b4f362d5d4244cd67b62cbe64967011f825473b
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-14 16:54:22 +0800

    add posts_controller def show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,25 @@ class PostsController < ApplicationController
   def  index
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
   def new
     @post = Post.new
   end
 
   def create
-    @post = Posts.new(post_params)
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path, notice: "新增留言成功!"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def post_params
-    psrams.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :tests
   resources :posts
-  # member do
-    # post :
+
 
   root 'posts#index'
 end
