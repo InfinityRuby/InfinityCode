779ac3a934072f6c0634fe570cbdbba3c6b456a6
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-14 14:29:29 +0800

    fix the file posts_controller add def new & create & private post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def  index
   end
+
+  def new
+    @post = Posts.new
+  end
+
+  def create
+    @post = Posts.new(post_params)
+  end
+
+  private
+  
+  def post_params
+    psrams.require(:post).permit(:title, :content)
+  end
 end
