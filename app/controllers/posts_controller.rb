b5069606e351c416911117e8c8ce8a526381d1e9
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-17 13:34:46 +0800

    fix postcontroller && view_show context

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
-  before_action：find_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def  index
   end
 
   def show
   end
+
   def new
     @post = Post.new
   end
@@ -20,19 +21,21 @@ class PostsController < ApplicationController
   end
 
   def edit
-    if @post.update
+  end
+
+  def update
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render 'edit'
     end
   end
-
-  def update
-  end
-
+  
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
-
+  
   def find_post
     @post = Post.find(params[:id])
   end
