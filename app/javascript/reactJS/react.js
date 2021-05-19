b026b153c380697aab10018f03afaf6cfb6ce013
eric <s51r3040@gmail.com>
2021-05-19 21:51:15 +0800

    新增文章關鍵字搜尋

--- app/javascript/reactJS/react.js
+++ app/javascript/reactJS/react.js
@@ -5,8 +5,20 @@ import {useEffect, useState} from "react"
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  function SearchForum(props) {
+    const {title, content} = props
+    return(
+      <div className="item">
+        <img src="https://picsum.photos/50/50?grayscale" alt="jpg" />
+        <div>      
+            <h2><a href="#">{title}</a></h2>
+            <h3>{content}</h3>
+        </div>
+      </div>
+    )
+  }
 
-  function LittleForum(props) {
+  function InitForum(props) {
     const {title, content} = props
     return(
       <div className="item">
@@ -20,12 +32,31 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function Forum() {
-    const [list, useList] = useState([])
+    const [list, setList] = useState([])
+    const [search, setSearch] = useState([]) 
+    const [change, setChange] = useState(true)
+
     useEffect(() => {
       fetch('/jsons/data')
       .then(res => res.json())
-      .then(post => useList(post))    
-  }, [])
+      .then(post => setList(post))    
+    }, [])
+
+    function todo(event) {
+      console.log(event.target.value[0])
+      if(event.key == 'Enter'){
+        setChange(false)
+        const current_search = list.filter(hash => hash.title.includes(event.target.value))
+        console.log(current_search)
+  
+        if(current_search != undefined){    
+          setSearch(current_search)
+        }
+      }
+      if(event.target.value == ''){
+        setChange(true)
+      }  
+    }
    
     return(
     <div>
@@ -42,15 +73,19 @@ document.addEventListener('DOMContentLoaded', () => {
               <a href="#">Most Votes</a>
             </div>
             <div>
-              <input type="text" id="forum_input" placeholder="Search topics or comments" />
+              <input type="text" id="forum_input" placeholder="Search topics or comments" onKeyDown={todo} />
               <button>New +</button>
             </div>
         </div>
         {
-          list.map((list) => {
-            return <LittleForum key={list.id} title={list.title} content={list.content} />
-          })
+          change ? list.map((list) => {
+            return <SearchForum key={list.id} title={list.title} content={list.content} /> 
+          }) :
+          search.map((search) => {
+            return <InitForum key={search.id} title={search.title} content={search.content} />
+          })  
         }
+        
     </div>
     )
   }
