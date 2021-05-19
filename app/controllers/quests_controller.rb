bdf1b02ac1b91d3e69d22359294fa96d49e9dfc9
shan hsu <ppearl0329tw@gmail.com>
2021-05-19 16:26:32 +0800

    新增: 串接資料庫題目Api和建立json的路徑網址

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,17 @@
 class QuestsController < ApplicationController
   def index
+    
   end
+  def show
+    @quests = Quest.all 
+    # find(params[:id])
+    respond_to do |format|
+      format.json { render json: @quests }
+      format.html { render :show }
+  end
+  end
+  def questdata
+    render json: {title: "test"}
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :quests
+  resources :quests 
+
+  get "/quests/questdata", to: "quests#questdata"
+
 end
