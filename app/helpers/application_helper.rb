cc0998a64d7b91abee9c5a2517051fde57da8428
shan hsu <ppearl0329tw@gmail.com>
2021-05-22 00:33:44 +0800

    刪除: redcarpet套件

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def markdown(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, {hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true}).render(text)
-  end
 end
