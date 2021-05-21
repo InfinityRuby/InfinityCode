89283e100a0ed803672838cebb0c23b3638db0dc
shan hsu <ppearl0329tw@gmail.com>
2021-05-22 02:43:51 +0800

    解決: 衝突

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def markdown(text)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, {hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true}).render(text)
+  end
 end
