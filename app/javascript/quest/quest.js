568f8f9e8000401038de6aed27faeeb99f274324
shan hsu <ppearl0329tw@gmail.com>
2021-05-22 02:35:51 +0800

    解決: 衝突

--- app/javascript/quest/quest.js
+++ app/javascript/quest/quest.js
@@ -55,20 +55,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementById(id);
     element.classList.add("active");
   })
-
-  const editor = CodeMirror.fromTextArea(document.querySelector("#editor"),{
-    mode: "ruby",
-    theme: "default",
-    lineNumbers: true,
-    smartIndent: true,
-    lineWrapping: true,
-    lineSeparator: '\n',
-    matchBrackets:true,
-  })
-  editor.setSize("100%","645")
-
-  const resetbtn = document.querySelector(".quest-footer-button:nth-child(1)")
-  resetbtn.addEventListener("click", (el)=>{
-    editor.setValue('')
-  })
 })
