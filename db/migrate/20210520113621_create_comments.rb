c4e780c351dcd9cdda25e5588a110824da8fe3f8
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-05-25 15:05:50 +0800

    修改: 資料表欄位 - 新增軟刪除 & 調整欄位名稱命名規則

--- db/migrate/20210520113621_create_comments.rb
+++ db/migrate/20210520113621_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.text :comment
+      t.text :content
       t.references :post, index: true
       t.references :user, index: true
 
