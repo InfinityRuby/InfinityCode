85671c44f5f395a9af96af389048980f87c773ff
TANG <tyu4520@gmail.com>
2021-05-25 16:03:36 +0800

    修正: 圖片上傳設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['aws_access_key_id'],     # required unless using use_iam_profile
-    aws_secret_access_key: ENV['aws_secret_access_key'],     # required unless using use_iam_profile
-    use_iam_profile:       true,                         # optional, defaults to false
-    region:                ENV['region'],                  # optional, defaults to 'us-east-1'
-    host:                  ENV['host'],             # optional, defaults to nil
+    provider:              'AWS',
+    aws_access_key_id:     ENV['aws_access_key_id'],
+    aws_secret_access_key: ENV['aws_secret_access_key'],
+    region:                ENV['region'],
+    host:                  ENV['host'],
   }
-  config.fog_directory  = ENV['name_of_bucket']          # required
+  config.fog_directory  = ENV['name_of_bucket']
+  config.fog_public     = false
 end
