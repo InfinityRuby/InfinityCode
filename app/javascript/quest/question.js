9e5916ea54c4be35e23cfcd7c8e572d25b163d4f
shan hsu <ppearl0329tw@gmail.com>
2021-05-29 01:05:47 +0800

    修改: 將監聽事件改為turblinks和調整語法排版

--- app/javascript/quest/question.js
+++ app/javascript/quest/question.js
@@ -8,7 +8,7 @@ import hljs from 'highlight.js'
 import 'highlight/styles/atelier-dune-dark.css'
 
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const tabs = document.querySelector(".quest-buttonWrapper");
   const tabButton = document.querySelectorAll(".quest-tab-button");
   const contents = document.querySelectorAll(".quest-content");
@@ -32,8 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const content = document.getElementById(id);
     content.classList.add("active");
-  }) 
-}
+    }) 
+  }
   if (document.querySelector("#editor")) {  
     const editor = CodeMirror.fromTextArea(document.querySelector("#editor"),{
       mode: "ruby",
@@ -44,42 +44,44 @@ document.addEventListener('DOMContentLoaded', () => {
       lineSeparator: '\n',
       matchBrackets:true,
       scrollbarStyle: null
-    })
+  })
     editor.setSize("500","500")
 
-    const html = marked('# Marked in Node.js\n\nRendered by **marked**.');
-    hljs.configure({
-      tabReplace: '  ',
-      classPrefix: 'hljs-',
-      languages: ['CSS', 'HTML, XML', 'JavaScript', 'PHP', 'Python', 'Stylus', 'TypeScript', 'Markdown']
-    })
+  const html = marked('# Marked in Node.js\n\nRendered by **marked**.');
+  hljs.configure({
+    tabReplace: '  ',
+    classPrefix: 'hljs-',
+    languages: ['CSS', 'HTML, XML', 'JavaScript', 'PHP', 'Python', 'Stylus', 'TypeScript', 'Markdown']
+  })
 
-    marked.setOptions({
-      renderer: new marked.Renderer(),
-      pedantic: false,
-      gfm: true,
-      breaks: false,
-      sanitize: false,
-      smartLists: true,
-      smartypants: false,
-      xhtml: false,
-      highlight: (code) => hljs.highlightAuto(code).value,
-    })
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    pedantic: false,
+    gfm: true,
+    breaks: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    xhtml: false,
+    highlight: (code) => hljs.highlightAuto(code).value,
+  })
 
-    fetch('/api/v1/quests')
-      .then(request => request.json())
-      .then(posts => {
-        const firstPost = posts[0]
-        editor.setValue(firstPost.problem)   
-        document.querySelector('.css-title').textContent = firstPost.title
-        document.querySelector('.css-level').textContent = firstPost.level
-        document.querySelector('.css-description').innerHTML = marked(firstPost.description)
-        document.querySelector('.css-picture').innerHTML = marked(firstPost.picture)
+  fetch('/api/v1/quests') 
+    .then(request => request.json())
+    .then(posts => {
+      const firstPost = posts[0]
+      editor.setValue(firstPost.problem)   
+      document.querySelector('.css-title').textContent = firstPost.title
+      document.querySelector('.css-level').textContent = firstPost.level
+      document.querySelector('.css-description').innerHTML = marked(firstPost.description)
+      document.querySelector('.css-picture').innerHTML = marked(firstPost.picture)
+    })
+  
+  const resetbtn = document.querySelector(".quest-footer-button:nth-child(1)")
+  if (resetbtn) {
+    resetbtn.addEventListener("click", (el)=> {
+      editor.setValue(firstPost.problem)
     })
   }
-        const resetbtn = document.querySelector(".quest-footer-button:nth-child(1)")
-        if (resetbtn) {resetbtn.addEventListener("click", (el)=>{
-          editor.setValue(firstPost.problem)
-          })
-        }
+  }
 })
