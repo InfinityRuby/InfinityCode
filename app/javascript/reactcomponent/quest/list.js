b7ebe73ca160251abc681876698f24e513f99e35
shan hsu <ppearl0329tw@gmail.com>
2021-05-28 23:17:16 +0800

    修改: 將解題頁面加上背景色和按鈕加深陰影

--- app/javascript/reactcomponent/quest/list.js
+++ app/javascript/reactcomponent/quest/list.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import {useEffect, useState, useRef} from 'react'
+import { useEffect, useState } from 'react'
 
 function ListQuest() {
   const [listQuest, setListQuest] = useState([])
