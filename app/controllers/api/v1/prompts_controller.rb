2fdd6f44d7951bd454b0c30755dc98b4231d9d3a
shan hsu <ppearl0329tw@gmail.com>
2021-05-30 17:19:01 +0800

    修改: 參數名稱

--- app/controllers/api/v1/prompts_controller.rb
+++ app/controllers/api/v1/prompts_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::PromptsController < ApiController
-  before_action :signed_in?
+  # before_action :signed_in?
 
   # 【GET】 查詢指定題目的提示列表  /api/v1/quests/:quest_id/prompts
   def index
-    @prompts = Prompt.where(quest_id: params[:quest_id])
+    @prompts = Prompt.where(questId: params[:quest_id])
 
     json_response(@prompts)
   end
 
   # 【GET】查詢指定題目的特定提示  /api/v1/quests/:quest_id/prompts/:id
   def show
-    @prompt = Prompt.where(quest_id: params[:quest_id], aseq: params[:id])
+    @prompt = Prompt.where(questId: params[:quest_id], aseq: params[:id])
 
     json_response(@prompt)
+    # render json: {a: 1}
   end
 end
