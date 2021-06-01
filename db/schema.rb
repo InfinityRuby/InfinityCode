7f1522afbde55c64f5a43bc33ea4c6c9b9c013a7
xiao-tang <10761101@gm.nfu.edu.tw>
2021-06-01 19:58:19 +0800

    修改: development 衝突

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 2021_05_30_130131) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.datetime "deleted_at"
     t.boolean "unknown", default: false
+    t.integer "user_id"
     t.index ["deleted_at"], name: "index_posts_on_deleted_at"
   end
 
