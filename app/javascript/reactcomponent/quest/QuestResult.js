5b8e8de2372cc86d8b25b9bf5b560c8ae4aef0e4
eric <s51r3040@gmail.com>
2021-06-02 16:09:05 +0800

    修改: 名稱錯誤修改

--- app/javascript/reactcomponent/quest/QuestResult.js
+++ app/javascript/reactcomponent/quest/QuestResult.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import API from '../discuss/lib/API'
 
 function Coin() {
   const [success, setSuccess] = useState(false)
-  const coinPrompt = document.querySelectorAll('.quest-prompt-button')
+  const [userCoins, setUserCoins] = useState([])
   const url = window.location.href
   const loc = url.substring(url.lastIndexOf('/') + 1 )
-  const reward = (status = true) => { setSuccess(status) }
+  const reward = (status = true) => { 
+    setSuccess(status)
+    API('POST', { coin_amount: userCoins.coin_amount + 5, 
+    coin_change: +5, description: "答題正確" }, 'coins')
+  }
+  
+  useEffect(() => {
+    fetch('/api/v1/coins')
+    .then(res => res.json())
+    .then(post => {
+      setUserCoins(post[post.length - 1])
+    })
+  }, [])
   
   return(
     <div>
@@ -25,16 +38,15 @@ function Coin() {
             <button 
             onClick={ () => { location.href = `/quests/${Number(loc) + 1}` } }
             className="quest-footer-button questbtn">
-              下ㄧ題
+              下一題
             </button>
           </div>
         </div>
       </div>
       : null }
       <div> 
-        <button className="quest-footer-button questbtn">Reset</button>
-        <button className="quest-footer-button questbtn">Submit</button>
-        <button onClick={ reward } className="quest-footer-button questbtn">Run</button>
+        <button className="quest-footer-button questbtn">重置</button>
+        <button onClick={ reward } className="quest-footer-button questbtn">送出</button>
       </div>
     </div>
   )
