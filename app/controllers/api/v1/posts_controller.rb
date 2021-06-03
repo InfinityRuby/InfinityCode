e6cadd80ef3eeb7fc1c246fe441cecdd26213d2c
xiao-tang <10761101@gm.nfu.edu.tw>
2021-06-04 00:02:31 +0800

    完成 討論區文章點讚功能

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PostsController < Api::V1::BaseController
-  before_action :find_post, only: [:update, :destroy]
+  before_action :find_post, only: [:total_votes, :update, :destroy]
   before_action :signed_in?, except: [:index, :show, :user]
 
   # 【GET】 查詢文章列表  /api/v1/posts
@@ -42,21 +42,18 @@ class Api::V1::PostsController < Api::V1::BaseController
   def upvote
     @post = Post.find(params[:id])
     @result = current_user.liked? @post
-    @post.like_by current_user
 
     if @result
       @post.unliked_by current_user
     else
       @post.liked_by current_user
     end
-    # redirect_to posts_path
-    render json: @result 
+    render json: {user_id: current_user.id, post_id: @post.id, liked: !@result}
   end
-  # 開一個布靈值 欄位
 
   def total_votes
     @total_like = @post.get_likes.size
-    render json: @total_like
+    render json: {total_like: @total_like}
   end
 
 
