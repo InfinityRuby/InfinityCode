48999ad2735c1aa87e13566f5c91ddd4187af5ea
shan hsu <ppearl0329tw@gmail.com>
2021-06-06 21:46:52 +0800

    修改: 路徑設計

--- app/controllers/api/v1/quests_controller.rb
+++ app/controllers/api/v1/quests_controller.rb
@@ -3,7 +3,28 @@ class Api::V1::QuestsController < Api::V1::BaseController
 
   # 【GET】查詢題目列表  /api/v1/quests
   def index
-    @quests = Quest.all 
+    # @quests = Quest.all 
+    # byebug
+
+    # if params[:level] && params[:title]
+    #   @quests = Quest.where(level: params[:level]).where(level: params[:level])
+    # elsif params[:level]
+    #   @quests = Quest.where(level: params[:level])
+    # elsif params[:title]
+    #   @quests = Quest.where(level: params[:level])
+    # else
+    #   @quests = Quest.all
+    # end
+
+
+    if params[:status] == "Success"
+      @quests = current_user.quests.distinct.where("answers.status = ?", "Success").with_level(params[:level])
+    elsif params[:status] == "Failure"
+      @quests = current_user.quests.distinct.where("answers.status = ?", "Failure").with_level(params[:level])
+    else
+      @quests = Quest.with_level(params[:level])
+    end
+
     json_response(@quests)
   end
 
@@ -13,20 +34,36 @@ class Api::V1::QuestsController < Api::V1::BaseController
     json_response(@quest)
   end
 
-  def easy
-    @quests = Quest.where(level: 'Easy')
+  # /api/v1/quests/easy
+  # /api/v1/quests?level=easy&status=Success
+  # /api/v1/quests?status=Success
+  # /api/v1/quests?level=easy
+
+
+  # def easy
+  #   @quests = Quest.where(level: 'Easy')
+  #   json_response(@quests)
+  # end
+
+  # def medium
+  #   @quests = Quest.where(level: 'Medium')
+  #   json_response(@quests)
+  # end
+
+  # def hard
+  #   @quests = Quest.where(level: 'Hard')
+  #   json_response(@quests)
+  # end
+
+  def solved
+    @quests = current_user.answers.where(status: 'Success')
     json_response(@quests)
   end
 
-  def medium
-    @quests = Quest.where(level: 'Medium')
-    json_response(@quests)
-  end
-
-  def hard
-    @quests = Quest.where(level: 'Hard')
-    json_response(@quests)
-  end
+  # def unsolved
+  #   @quests = current_user.answers.where(status: 'Failure')
+  #   json_response(@quests)
+  # end
 
   # 【POST】新增答題記錄  /api/v1/quests/:id/answer
   # body: { type: 'ruby', answer: '答題內容' }
