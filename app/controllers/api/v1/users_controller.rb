0009530d42c3153ab052fb29d3ac78a7580a0228
shan hsu <ppearl0329tw@gmail.com>
2021-06-06 22:27:37 +0800

    修改: 調整段落

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,10 +9,8 @@ class Api::V1::UsersController < Api::V1::BaseController
     total_solved = current_user.answers.where(status: 'Success').count
     
     level_solved= current_user.quests.distinct.where('answers.status = ?', "Success").group(:level).count
-    
     level_total = Quest.group(:level).count
-    #{"Hard"=>2, "Medium"=>5, "Easy"=>8} 
-
+    
     total_percentage = (total_solved / total_quest.to_f).round(2)
     easy_percentage = (level_solved["Easy"] / level_total["Easy"].to_f).round(2)
     medium_percentage = (level_solved["Medium"] / level_total["Medium"].to_f).round(2)
