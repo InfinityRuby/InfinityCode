2ff1a5ec5ffcc165e161a5d2f75858599c0006f4
shan hsu <ppearl0329tw@gmail.com>
2021-06-08 00:17:58 +0800

    修改: 把註解刪除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,12 @@
-# # This file should contain all the record creation needed to seed the database with its default values.
-# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-# #
-# # Examples:
-# #
-# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-# #   Character.create(name: 'Luke', movie: movies.first)
-# Quest.create!(
-#   title: 'Hello InfinityCode!',
-#   level: 'Easy',
-#   description: 'Let\'s get started with Ruby!<br />Go ahead and type the following code in the code-editor:<br />`print "Hello InfinityCode!!`"',
-#   picture: '',
-#   problem: '',
-#   created_at: Time.now,
-#   updated_at: Time.now
-# )
+Quest.create!(
+  title: 'Hello InfinityCode!',
+  level: 'Easy',
+  description: 'Let\'s get started with Ruby!<br />Go ahead and type the following code in the code-editor:<br />`print "Hello InfinityCode!!`"',
+  picture: '',
+  problem: '',
+  created_at: Time.now,
+  updated_at: Time.now
+)
 Quest.create!(
   title: 'Two Sum',
   level: 'Easy',
