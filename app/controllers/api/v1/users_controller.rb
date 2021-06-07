591dfb9cfca2ec9a1db480c559b1b6dd05210858
shan hsu <ppearl0329tw@gmail.com>
2021-06-07 16:19:58 +0800

    修改: 段落調整、新增已解決總題數的語法

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,13 +8,14 @@ class Api::V1::UsersController < Api::V1::BaseController
     total_quest = Quest.count
     total_solved = current_user.answers.where(status: 'Success').count
     
+    quest_solved = current_user.quests.distinct.where('answers.status = ?', "Success").count
     level_solved= current_user.quests.distinct.where('answers.status = ?', "Success").group(:level).count
     level_total = Quest.group(:level).count
     
-    total_percentage = (total_solved / total_quest.to_f).round(2)
+    total_percentage = (quest_solved / total_quest.to_f).round(2)
     easy_percentage = (level_solved["Easy"] / level_total["Easy"].to_f).round(2)
     medium_percentage = (level_solved["Medium"] / level_total["Medium"].to_f).round(2)
-    hard_percentage = (level_solved["Hard"] / level_total["Hard"].to_f).round(2)
+    hard_percentage = (level_solved["Hard"]/ level_total["Hard"].to_f).round(2)
 
     json_response({
       total_percentage: total_percentage,
