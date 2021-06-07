27cdb8cf798f375cdcc72df01238ca2953444db3
eric <s51r3040@gmail.com>
2021-06-07 18:31:30 +0800

    修改: 整合新的API串接留言板

--- app/javascript/component/discuss/comments/Pages.js
+++ app/javascript/component/discuss/comments/Pages.js
@@ -1,24 +1,18 @@
 import React from 'react'
 
-export default function Pages({ commentsAPI, commentPages, setCommentPages }) {
+export default function Pages({ comments, commentPages, setCommentPages }) {
   const previousPage = () => commentPages > 1 && setCommentPages(commentPages - 1)  
-  const nextPage = () => commentPages * 6 < commentsAPI.length && setCommentPages(commentPages + 1)
-  const changePage = event => commentPages * 6 < commentsAPI.length + 6 && setCommentPages(Number(event.target.textContent))
-  const lastPage = event => setCommentPages(Number(event.target.textContent))
-  const returnPage = () => setCommentPages(1)
+  const nextPage = () => setCommentPages(commentPages + 1)
+  const changePage = event => setCommentPages(Number(event.target.textContent))
 
   return(
     <div className="single-article-page">
       <span onClick={ previousPage }><i className="fas fa-chevron-left"></i></span>
-      { commentPages >= 5 ? <span onClick={ returnPage }>{ 1 }</span> : null }
-      { commentPages >= 5 ? <h5>...</h5> : null }
-      { commentPages * 6 < commentsAPI.length + 6 ? <span className="first-button-color" onClick={ changePage }>{ commentPages }</span> : null }
-      { commentPages * 6 < commentsAPI.length - 6 ?  <span id="2" onClick={ changePage }>{ commentPages + 1 }</span> : null }
-      { commentPages * 6 < commentsAPI.length -12 ? <span id="3" onClick={ changePage }>{ commentPages + 2 }</span> : null }
-      { commentPages * 6 < commentsAPI.length - 18 ? <span id="4" onClick={ changePage }>{ commentPages + 3 }</span> : null }
-      { commentPages * 6 < commentsAPI.length - 24 ? <span id="5" onClick={ changePage }>{ commentPages + 4 }</span> : null }
-      { commentPages < Math.ceil(commentsAPI.length / 6) ? <h5>...</h5> : null }
-      { commentPages < Math.ceil(commentsAPI.length / 6) ? <span onClick={ lastPage }>{ Math.ceil(commentsAPI.length / 6) }</span> : null }
+      <span className="first-button-color" onClick={ changePage }>{ commentPages }</span>
+      <span onClick={ changePage }>{ commentPages + 1 }</span>
+      <span onClick={ changePage }>{ commentPages + 2 }</span>
+      <span onClick={ changePage }>{ commentPages + 3 }</span>
+      <span onClick={ changePage }>{ commentPages + 4 }</span>
       <span onClick={ nextPage }><i className="fas fa-chevron-right"></i></span>
     </div>
   )
