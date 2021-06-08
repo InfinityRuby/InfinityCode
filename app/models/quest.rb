fc423cd22df1266860e2d8a12d4582a127a2a456
shan hsu <ppearl0329tw@gmail.com>
2021-06-08 16:48:04 +0800

    修改: 修改箭頭後的空格

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,5 +2,5 @@ class Quest < ApplicationRecord
   has_many :answers
   has_many :cases
   has_many :prompts
-  scope :with_level, ->(data) { where(level: data) if data }
+  scope :with_level, -> (data) { where(level: data) if data }
 end
