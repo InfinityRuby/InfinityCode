ff49093adcd488bdef7844c5598515f361f292e0
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-06-09 01:06:24 +0800

    修改: 調整 API 的註解說明寫法

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,29 +2,34 @@ class Api::V1::PostsController < Api::V1::BaseController
   before_action :find_post, only: [:update, :destroy]
   before_action :signed_in?, except: [:index, :show, :user]
 
-  # 【GET】 查詢文章列表  /api/v1/posts
+  # 查詢文章列表  
+  # GET: /api/v1/posts
   def index
   end
 
-  # 【POST】 新增文章  /api/v1/posts
-  # body: { title: '標題', content: '內容' }
+  # 新增文章  
+  # POST: /api/v1/posts
+  # params: { title: '標題', content: '內容' }
   def create
     @post = current_user.posts.create!(post_params)
     json_response(@post, :created)
   end
 
-  # 【GET】 查詢指定文章  /api/v1/posts/:id
+  # 查詢指定文章  
+  # GET: /api/v1/posts/:id
   def show
   end
 
-  # 【PUT】 編輯指定文章  /api/v1/posts/:id
-  # body: { title: '標題', content: '內容' }
+  # 編輯指定文章  
+  # PUT: /api/v1/posts/:id
+  # params: { title: '標題', content: '內容' }
   def update
     @post.update(post_params)
     head :no_content
   end
 
-  # 【DELETE】 刪除指定文章  /api/v1/posts/:id
+  # 刪除指定文章  
+  # DELETE: /api/v1/posts/:id
   def destroy
     @post.destroy
     head :no_content
