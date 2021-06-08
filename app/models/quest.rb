2301a46a7b9d6ab0b3c947a849c0c3b010c4e20a
shan hsu <ppearl0329tw@gmail.com>
2021-06-08 10:58:42 +0800

    修改: 將重複的Scope刪除

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -3,13 +3,4 @@ class Quest < ApplicationRecord
   has_many :cases
   has_many :prompts
   scope :with_level, ->(data) { where(level: data) if data }
-
-  # scope :open_public, -> { where( :is_public => true ) }
-  def self.with_level(data)
-    if data
-      where(level: data)
-    else
-      all
-    end
-  end
 end
