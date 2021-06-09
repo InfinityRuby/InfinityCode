1b1bca240874e6239da2305306f3d8b97fcd0fd4
eric <s51r3040@gmail.com>
2021-06-10 00:31:36 +0800

    修改: 討論區的頁籤功能，API串接

--- app/javascript/component/discuss/comments/Pages.js
+++ app/javascript/component/discuss/comments/Pages.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
-export default function Pages({ comments, commentPages, setCommentPages }) {
+export default function Pages({ commentPages, setCommentPages, maxPage }) {
   const previousPage = () => commentPages > 1 && setCommentPages(commentPages - 1)  
-  const nextPage = () => setCommentPages(commentPages + 1)
+  const nextPage = () => commentPages < maxPage && setCommentPages(commentPages + 1)
   const changePage = event => setCommentPages(Number(event.target.textContent))
 
   return(
     <div className="single-article-page">
       <span onClick={ previousPage }><i className="fas fa-chevron-left"></i></span>
+      { commentPages >= 4 ? <span onClick={ changePage }>1</span> : null }
+      { commentPages >= 4 ? <h5>...</h5> : null }
       <span className="first-button-color" onClick={ changePage }>{ commentPages }</span>
-      <span onClick={ changePage }>{ commentPages + 1 }</span>
-      <span onClick={ changePage }>{ commentPages + 2 }</span>
-      <span onClick={ changePage }>{ commentPages + 3 }</span>
-      <span onClick={ changePage }>{ commentPages + 4 }</span>
+      { commentPages + 1 < maxPage ? <span onClick={ changePage }>{ commentPages + 1 }</span> : null }
+      { commentPages + 2 < maxPage ? <span onClick={ changePage }>{ commentPages + 2 }</span> : null }
+      { commentPages + 3 < maxPage ? <span onClick={ changePage }>{ commentPages + 3 }</span> : null }
+      { commentPages + 4 < maxPage ? <span onClick={ changePage }>{ commentPages + 4 }</span> : null }
+      { commentPages + 4 < maxPage ? <h5>...</h5> : null }
+      { commentPages < maxPage ? <span onClick={ changePage }>{ maxPage }</span> : null }
       <span onClick={ nextPage }><i className="fas fa-chevron-right"></i></span>
     </div>
-  )
+  ) 
 }
--- app/javascript/component/discuss/list/Pages.js
+++ app/javascript/component/discuss/list/Pages.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
-export default function Pages({ list, initPage, setInitPage }) {
-  const nextPage = number => setInitPage(initPage + number)
+export default function Pages({ initPage, setInitPage, maxPage }) {
+  const nextPage = number => initPage < maxPage && setInitPage(initPage + number)
   const previousPage = () => initPage > 1 && setInitPage(initPage - 1)
   const searchPage = event => {
     if(event.key == 'Enter'){
-      if(Number(event.target.value) && Number(event.target.value) * 10 <= list.length + 10){
+      if(Number(event.target.value) && Number(event.target.value) <= maxPage){
         setInitPage(Number(event.target.value))
+        event.target.value = ''
       }
     }
   }
-
   const changePage = event => setInitPage(Number(event.target.textContent))
 
   return(
     <div className="pagination">
       <button onClick={ previousPage }>上一頁</button>
       <button onClick={ changePage }>{ initPage }</button>
-      <button onClick={ changePage }>{ initPage + 1}</button>
-      <button>{ initPage + 2 }</button>
-      <button onClick={ changePage }>{ initPage + 3 }</button>
-      <button onClick={ changePage }>{ initPage + 4 }</button>
+      { initPage + 1 <= maxPage ? <button onClick={ changePage }>{ initPage + 1}</button> : null }
+      { initPage + 2 <= maxPage ? <button>{ initPage + 2 }</button> : null }
+      { initPage + 3 <= maxPage ? <button onClick={ changePage }>{ initPage + 3 }</button> : null }
+      { initPage + 4 <= maxPage ? <button onClick={ changePage }>{ initPage + 4 }</button> : null }
       <button onClick={ () => nextPage(1) }>下一頁</button>
       <input type="text" onKeyDown={ searchPage } placeholder={ "目前在第"+initPage+"頁" } />
       <button onClick={ () => setInitPage(1) }>回首頁</button>
