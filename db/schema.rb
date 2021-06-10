59017c9fa5378579fdc4fd920ebedd0e8a7d2ae7
XiaoTnag <fengyue729@gmail.com>
2021-06-10 17:40:39 +0800

    修改: 移除與匿名新增無用的migrate

--- db/schema.rb
+++ db/schema.rb
@@ -53,9 +53,8 @@ ActiveRecord::Schema.define(version: 2021_06_11_073033) do
 
   create_table "coins", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "coin_amount", default: 20
     t.integer "coin_change", default: 0
-    t.string "description", default: "新用戶註冊禮"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_coins_on_user_id"
@@ -83,6 +82,23 @@ ActiveRecord::Schema.define(version: 2021_06_11_073033) do
     t.integer "user_id"
     t.datetime "deleted_at"
     t.boolean "anonymous", default: false
+<<<<<<< HEAD
+=======
+=======
+    t.boolean "unknown", default: false
+<<<<<<< HEAD
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
+    t.boolean "publish_author", default: false
+>>>>>>> 完成: 新增討論區文章 匿名發文API
+=======
+>>>>>>> 修改: 移除與匿名新增無用的migrate
+>>>>>>> 修改: 移除與匿名新增無用的migrate
     t.index ["deleted_at"], name: "index_posts_on_deleted_at"
   end
 
@@ -142,7 +158,10 @@ ActiveRecord::Schema.define(version: 2021_06_11_073033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+<<<<<<< HEAD
   add_foreign_key "achievements", "badges"
   add_foreign_key "achievements", "users"
+=======
+>>>>>>> 修改: 移除與匿名新增無用的migrate
   add_foreign_key "profiles", "users"
 end
