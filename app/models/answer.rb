82677c145e1c7d886f8ce416b19ce8186fb541bd
shan hsu <ppearl0329tw@gmail.com>
2021-06-10 18:48:39 +0800

    修改: 金幣欄位變動

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,20 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :quest
 
-  
+  before_create :change_coin
+
+  def change_coin
+    return if user.quests.where('answers.status = ?', "Success").include? quest   
+
+    if quest.level == "Easy"
+      user.update(coin_amount: user.coin_amount + 5)
+
+    elsif quest.level == "Medium"
+      user.update(coin_amount: user.coin_amount + 10)
+
+    elsif quest.level == "Hard"
+      user.update(coin_amount: user.coin_amount + 15)
+    end
+  end
 end
+
