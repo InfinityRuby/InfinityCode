4e7658bbb8f5895b94bf77ead73c7b4526e725d7
shan hsu <ppearl0329tw@gmail.com>
2021-06-10 18:43:58 +0800

    修改: 將:create刪除

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController 
   def index
-    json_response(current_user, :created)
+    json_response(current_user)
   end
 
   # 查詢特定使用者的解題完成率
@@ -11,6 +11,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     quest_solved = current_user.quests.distinct.where('answers.status = ?', "Success").count
     level_solved = current_user.quests.distinct.where('answers.status = ?', "Success").group(:level).count
     level_total = Quest.group(:level).count
+
     
     total_percentage = (quest_solved / total_quest.to_f).round(2)
     easy_percentage = (level_solved["Easy"] / level_total["Easy"].to_f).round(2)
