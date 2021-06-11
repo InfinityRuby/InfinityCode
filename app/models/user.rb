44b12f0adcafe3c83bbce62f4b903dc7400c3348
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-06-11 20:19:12 +0800

    新增: 獎章模組 - 貼文、留言、會員、解題 (#190)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,12 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :answers
   has_many :quests, through: :answers
+  has_many :achievements
 
   after_create do
     create_profile
   end
+  after_commit :check_user_achievement
 
   before_create do
     set_default_value
@@ -29,5 +31,30 @@ class User < ApplicationRecord
   private
   def set_default_value
     self.coin_amount = 20
+  end
+
+  # 檢查成就 - 註冊天數
+  def check_user_achievement
+    last_sign_in = self.last_sign_in_at.to_date
+    created_at = self.created_at.to_date
+
+    if (last_sign_in != nil and created_at != nil)
+      diff_day = (last_sign_in - created_at).to_i
+      
+      case diff_day
+      when 10
+        badge_id = 13
+      when 50
+        badge_id = 14
+      when 100
+        badge_id = 15
+      when 500
+        badge_id = 16
+      end
+    end
+    
+    @user = User.find(self.id)
+
+    create_achievement(badge_id)
   end
 end
