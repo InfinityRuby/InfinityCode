54d3044d8f1fae3f5e88ddd211688798b3a0b1db
shan hsu <ppearl0329tw@gmail.com>
2021-06-12 12:35:40 +0800

    修改: 網址名稱

--- app/controllers/api/v1/ranks_controller.rb
+++ app/controllers/api/v1/ranks_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::RanksController < Api::V1::BaseController
-  #api/v1/ranks
+  #api/v1/ranks/coin
   def coin
     @users = User.limit(10).order('coin_amount desc')
   end
 
-  #api/v1/ranks/post_rank
+  #api/v1/ranks/post
   def post
     @user_posts_count = Post.unscope(:order).group(:user_id).order('COUNT(id) DESC').limit(10).count(:id)
     user_ids = @user_posts_count.keys
@@ -12,7 +12,7 @@ class Api::V1::RanksController < Api::V1::BaseController
     @users = User.includes(:profile).where(id: user_ids).sort { |a, b| user_ids.index(a.id) <=> user_ids.index(b.id) }
   end
 
-  #api/v1/ranks/comment_rank
+  #api/v1/ranks/comment
   def comment
     @user_comments_count = Comment.unscope(:order).group(:user_id).order('COUNT(id) DESC').limit(10).count(:id)
     user_ids = @user_comments_count.keys
