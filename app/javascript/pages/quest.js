807c777d48e341394e0974b40beb2ba05064ed39
TANG <tyu4520@gmail.com>
2021-06-14 02:13:02 +0800

    修改: 題目百分比浮點數

--- app/javascript/pages/quest.js
+++ app/javascript/pages/quest.js
@@ -5,7 +5,7 @@ document.addEventListener('turbolinks:load', () => {
     fetch(`/api/v1/users/1/completed_rate`)
     .then(request => request.json())
     .then(percent => {
-      const totalPercent = percent.total_percentage * 100
+      const totalPercent = (percent.total_percentage * 100).toFixed(0)
       progressBar.innerHTML = `<div class="progress-filler" style="hight: 0.5rem; width: ${totalPercent}%; background-color: #929292"></div>`        
       percentText.textContent = `${totalPercent} %`
     })
