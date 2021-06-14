0d1d6ae0475a985e6ee2e7448ce986d66d2faed2
shan hsu <ppearl0329tw@gmail.com>
2021-06-14 14:32:52 +0800

    修改: 將多餘資料刪除

--- app/controllers/api/v1/problems_controller.rb
+++ app/controllers/api/v1/problems_controller.rb
@@ -6,8 +6,6 @@ class Api::V1::ProblemsController < Api::V1::BaseController
   
   #api/v1/problems/id
   def show
-    @problem = current_user.quests.find(params[:id]).answers.where(:status => 'Success' ,:user_id => current_user)
     
-    @problems = Quest.find(params[:id]).answers.where(status: 'Success').order(created_at: :desc)
   end
 end
