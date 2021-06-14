721b3cbf344c3fedb9a22eafa11d6aaeb24110d4
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-06-14 16:20:51 +0800

    修改: JS 語法重構 & 錯誤命名修正

--- app/javascript/pages/disscuss.js
+++ app/javascript/pages/disscuss.js
@@ -1,86 +1,30 @@
 document.addEventListener('turbolinks:load', () => {
-  const first = document.querySelector('.first')
-  const second = document.querySelector('.second')
-  const third = document.querySelector('.third')
-  const fourth = document.querySelector('.fourth')
+  const items = document.querySelectorAll('.discuss-list');
 
-
-
-  if(first && second && third && fourth) {
+  if(items) {
     fetch('api/v1/posts')
-      .then(request => request.json())
+      .then(res => res.json())
+      .then(obj => obj.posts)
       .then(posts => {
-        const post = posts.posts    
-        const idFirst = post[0].id
-        const titleFirst = post[0].title.slice(0, 6)
-        const contentFirst = post[0].content.slice(0, 6)
-        const nameFirst = post[0].author.name
-        const avatarFirst = post[0].author.avatar
-        
-        const idSecond = post[1].id
-        const titleSecond = post[1].title.slice(0, 6)
-        const contentSecond = post[1].content.slice(0, 6)
-        const nameSecond = post[1].author.name
-        const avatarSecond = post[1].author.avatar
+        items.forEach((item, index) => {
+          const post = posts[index];
+          const id = post.id;
+          const title = post.title.slice(0, 6);
+          const content = post.content.slice(0, 6);
+          const name = post.author.name;
+          const avatar = post.author.avatar;
 
-        const idThird = post[2].id
-        const titleThird = post[2].title.slice(0, 6)
-        const contentThird = post[2].content.slice(0, 6)
-        const nameThird = post[2].author.name
-        const avatarThird = post[2].author.avatar
-
-        const idFourth = post[3].id
-        const titleFourth = post[3].title.slice(0, 6)
-        const contentFourth = post[3].content.slice(0, 6)
-        const nameFourth = post[3].author.name
-        const avatarFourth = post[3].author.avatar
-  
-        first.innerHTML = 
-        `<div class="post-content">
-          <div class="pic"><img src="${avatarFirst}" alt="jpg" /></div>
+          item.innerHTML = `<div class="post-content">
+          <div class="pic"><img src="${avatar}" alt="jpg" /></div>
         <div>
-          <h2><a href="/posts/${idFirst}">${titleFirst}</a></h2>
-          <h3 class="content-1">${contentFirst}...</h3>
+          <h2><a href="/posts/${id}">${title}</a></h2>
+          <h3 class="content-1">${content}...</h3>
         </div>
         </div>
         <div class="author">
-          <div>文章作者: <span>${nameFirst}</span></div>
-        </div></div>`
-        second.innerHTML = 
-        `<div class="post-content">
-          <div class="pic"><img src="${avatarSecond}" alt="jpg" /></div>
-        <div>
-          <h2><a href="/posts/${idSecond}">${titleSecond}</a></h2>
-          <h3 class="content-1">${contentSecond}...</h3>
-        </div>
-        </div>
-        <div class="author">
-          <div>文章作者: <span>${nameSecond}</span></div>
-        </div></div>`
-        third.innerHTML = 
-        `<div class="post-content">
-          <div class="pic"><img src="${avatarThird}" alt="jpg" /></div>
-        <div>
-          <h2><a href="/posts/${idThird}">${titleThird}</a></h2>
-          <h3 class="content-1">${contentThird}...</h3>
-        </div>
-        </div>
-        <div class="author">
-          <div>文章作者: <span>${nameThird}</span></div>
-        </div></div>`
-        fourth.innerHTML = 
-        `<div class="post-content">
-          <div class="pic"><img src="${avatarFourth}" alt="jpg" /></div>
-        <div>
-          <h2><a href="/posts/${idFourth}">${titleFourth}</a></h2>
-          <h3 class="content-1">${contentFourth}...</h3>
-        </div>
-        </div>
-        <div class="author">
-          <div>文章作者: <span>${nameFourth}</span></div>
-        </div></div>`
-    })
+          <div>文章作者: <span>${name}</span></div>
+        </div></div>`;
+        });
+      });
   }
-})
-
-
+});
--- app/javascript/pages/rank.js
+++ app/javascript/pages/rank.js
@@ -1,72 +1,41 @@
 document.addEventListener('turbolinks:load', () => {
   const ranks = document.querySelector(".rank-content");
   const rankBtn = document.querySelectorAll(".rank-button");
-  const rankContents = document.querySelectorAll(".content");
-  const coinNameFirst = document.querySelector('.coin-1');
-  const coinNameSecond = document.querySelector('.coin-2');
-  const coinNameThird = document.querySelector('.coin-3');
-  const postNameFirst = document.querySelector('.post-1');
-  const postNameSecond = document.querySelector('.post-2');
-  const postNameThird = document.querySelector('.post-3');
-  const likeNameFirst = document.querySelector('.like-1');
-  const likeNameSecond = document.querySelector('.like-2');
-  const likeNameThird = document.querySelector('.like-3');
-  const questNameFirst = document.querySelector('.quests-1');
-  const questNameSecond = document.querySelector('.quests-2');
-  const questNameThird = document.querySelector('.quests-3');
 
   if(ranks) {
-    ranks.onclick = e => {
-      const id = e.target.dataset.id;
-      if (id) {
-        rankBtn.forEach(btn => {
-          btn.classList.remove("toggle");
-        });
-        e.target.classList.add("toggle");
-    
-        rankContents.forEach(content => {
-          content.classList.remove("toggle");
-        });
-        const element = document.getElementById(id);
-        element.classList.add("toggle");
-      }
-    }
-  }  
-  if (coinNameFirst && coinNameSecond && coinNameThird) {
-  fetch('./api/v1/ranks/coins')
-    .then(request => request.json())
-    .then(coins => {
-      coinNameFirst.innerHTML = `<i class="fas fa-trophy gold"></i><span class="name">${coins[0].profile.name}</span><span class="count">${coins[0].score}</span>`
-      coinNameSecond.innerHTML = `<i class="fas fa-trophy sliver"></i><span class="name">${coins[1].profile.name}</span><span class="count">${coins[1].score}</span>`
-      coinNameThird.innerHTML = `<i class="fas fa-trophy copper"></i><span class="name">${coins[2].profile.name}</span><span class="count">${coins[2].score}</span>`     
-    })
-  }
-  if (postNameFirst && postNameSecond && postNameThird) {
-  fetch('./api/v1/ranks/posts')
-    .then(request => request.json())
-    .then(posts => {
-      postNameFirst.innerHTML = `<i class="fas fa-trophy gold"></i><span class="name">${posts[0].profile.name}</span><span class="count">${posts[0].score}</span>`
-      postNameSecond.innerHTML = `<i class="fas fa-trophy sliver"></i><span class="name">${posts[1].profile.name}</span><span class="count">${posts[1].score}</span>`
-      postNameThird.innerHTML = `<i class="fas fa-trophy copper"></i><span class="name">${posts[2].profile.name}</span><span class="count">${posts[2].score}</span>`
-    })
-  }
-  if (likeNameFirst && likeNameSecond && likeNameThird) {
-  fetch('./api/v1/ranks/likes')
-    .then(request => request.json())
-    .then(likes => {
-      likeNameFirst.innerHTML = `<i class="fas fa-trophy gold"></i><span class="name">${likes[0].profile.name}</span><span class="count">${likes[0].score}</span>`
-      likeNameSecond.innerHTML = `<i class="fas fa-trophy sliver"></i><span class="name">${likes[1].profile.name}</span><span class="count">${likes[1].score}</span>`
-      likeNameThird.innerHTML = `<i class="fas fa-trophy copper"></i><span class="name">${likes[2].profile.name}</span><span class="count">${likes[2].score}</span>`
-    })
-  }
-  if (questNameFirst && questNameSecond && questNameThird) {
-    fetch('./api/v1/ranks/solved')
-      .then(request => request.json())
-      .then(solved => {
-        questNameFirst.innerHTML = `<i class="fas fa-trophy gold"></i><span class="name">${solved[0].profile.name}</span><span class="count">${solved[0].score}</span>`
-        questNameSecond.innerHTML = `<i class="fas fa-trophy sliver"></i><span class="name">${solved[1].profile.name}</span><span class="count">${solved[1].score}</span>`
-        questNameThird.innerHTML = `<i class="fas fa-trophy copper"></i><span class="name">${solved[2].profile.name}</span><span class="count">${solved[2].score}</span>`
-      })
-    }
-})
+    // Function - 排行榜 API 串接
+    const dataRender = (e) => {
+      let toggledBtn = document.querySelector('button.toggle');
+      let toggledDiv = document.querySelector('div.toggle');
+      let type;
 
+      if(e) {
+        toggledBtn.classList.remove('toggle');
+        e.target.classList.add('toggle');
+
+        toggledDiv.classList.remove('toggle');
+        type = e.target.dataset.id;
+        document.querySelector(`div#${type}`).classList.add('toggle');
+      } else {
+        type = toggledBtn.dataset.id;
+      }
+
+      fetch(`/api/v1/ranks/${type}`)
+        .then(res => res.json())
+        .then(obj => {
+          obj.forEach((data, index) => {
+            const item = document.querySelector(`.${type}-${index + 1}`);
+            const color = ['gold', 'silver', 'copper'];
+
+            item.innerHTML = `<i class="fas fa-trophy ${color[index]}"></i><span class="name">${data.profile.name}</span><span class="count">${data.score}</span>`;
+          });
+        });
+    }
+
+    dataRender();
+
+    rankBtn.forEach(btn => {
+      btn.addEventListener('click', dataRender);
+    });
+  }
+});
