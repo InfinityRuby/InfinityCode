4a0c2cb40a151e990ef7369be623ccc293cedf6f
TANG <tyu4520@gmail.com>
2021-06-15 11:47:07 +0800

    調整: postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,47 @@
-let environment = {
+module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
+    })
   ]
 }
 
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/**/*.html.erb',
-        './app/**/**/*.rb',
-        './app/**/**/*.js',
-        './app/**/**/*.scss',
-        './app/**/**/*.jsx',
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
+// 今天 demo 完會再做處理
+// let environment = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//     require('postcss-import'),
+//     require('postcss-flexbugs-fixes'),
+//     require('postcss-preset-env')({
+//       autoprefixer: {
+//         flexbox: 'no-2009'
+//       },
+//       stage: 3
+//     }),
+//   ]
+// }
 
-module.exports = environment
+// if (process.env.RAILS_ENV === "production") {
+//   environment.plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: [
+//         './app/**/**/*.html.erb',
+//         './app/**/**/*.rb',
+//         './app/**/**/*.js',
+//         './app/**/**/*.scss',
+//         './app/**/**/*.jsx',
+//       ],
+//       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//     })
+//   )
+// }
+
+// module.exports = environment
